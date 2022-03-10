eae98a81dce3f699ce693d8fb3f7c00fe53944b1
unknown <slagalo39@gmail.com>
2022-03-10 10:28:46 +0100

    Dodani setteri i getteri uz poneku metodu

--- Zadaca1/Calculator.java
+++ Zadaca1/Calculator.java
@@ -1,9 +1,8 @@
 import java.util.*;
 public class Calculator {
-    private
+    private:
     double a,b;
     final double PI=Math.atan(1)*4;
-
 
 //private Calculator(){}
 private Calculator(){
@@ -17,18 +16,28 @@ private Calculator(double a, double b){
 private Calculator(double x){
     this(x,0);
 }
+public void setOperandA(double a){this.a=a;}
+    public void setOperandB(){this.b=b;}
+public void setOperandsAandB(){}
+    public double getOperandA(){return a;}
+    public double getOperandB(){return b;}
 public void writeThisOut(){
     System.out.println("Bgm ovo postaje polako jedan fin kalkulatorcic"+this.a+" "+this.b);
-
 }
 public double Add(double a, double b){
+return this.a+this.b;
+}
+public double Divide(double a,double b){// uzeo sam double da izbjegnemo potrebu za obilazenjem dijeljenja s nulom
+        return this.a/this.b;
+    }
+public double Subtract(double a,double b){
 
 }
-public  double
-
     public static void main(String[] args) {
     Calculator obj=new Calculator(0,4);
     System.out.println(obj.a+" "+obj.b);
+
+    obj.writeThisOut();
     //System.exit(0);
 
     }
